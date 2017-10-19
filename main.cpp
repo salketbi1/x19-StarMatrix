fc4ded0b0e4bca6c56b9cfdf6042bfb05090e840
salketbi1 <salketbi1@mail.csuchico.edu>
2017-10-18 18:50:33 -0700

    x19

---
+++ main.cpp
@@ -0,0 +1,45 @@
+//Saif Alketbi
+//CSCI 111
+//x19
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  vector<vector<char> > stars;
+  int row;
+  int col;
+
+  cout << "Enter rows and columns of stars:" << endl;
+
+  cin >> row;
+  stars.resize(row);
+
+  cin >> col;
+
+  for (int i=0;i<row;i++)
+  {
+    stars[i].resize(col);
+
+    for (int j=0;j<col;j++)
+    {
+      stars[i][j]='*';
+    }
+  }
+
+  // print the matrix
+  for (int i=0;i<row;i++)
+  {
+    for (int j=0;j<col;j++)
+    {
+      cout << stars[i][j];
+    }
+
+    cout << "\n";
+  }
+
+  return 0;
+}
